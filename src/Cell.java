30486d4b75ae36b1c056b4bc70e933dc2506edd2
shaharhamish <shaharhamish@gmail.com>
2024-12-26 16:03:10 +0200

    Start working on Cell department

--- src/Cell.java
+++ src/Cell.java
@@ -1,3 +1,20 @@
 public class Cell {
+    private String value;
 
+    public Cell(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+
+    public void setValue(String value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Cell{" + "value='" + value + '\'' + '}';
+    }
 }
