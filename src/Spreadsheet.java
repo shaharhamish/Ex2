5a2c7b8f9fe9ef20ee363ef7830c66d0f8e2705e
shaharhamish <shaharhamish@gmail.com>
2024-12-26 17:24:51 +0200

    Adding constructor to Spreadsheet

--- src/Spreadsheet.java
+++ src/Spreadsheet.java
@@ -1,4 +1,12 @@
 public class Spreadsheet {
     private Cell[][] cells;
-
+    //constructor
+    public Spreadsheet(int width, int height) {
+        cells = new Cell[width][height];
+        for (int i = 0; i < width; i++) {
+            for (int j = 0; j < height; j++) {
+                cells[i][j] = new Cell("");
+            }
+        }
+    }
 }
